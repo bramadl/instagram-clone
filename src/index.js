49a1f50d62b186a7db92268d0fe45e6bc9bcb0d5
Bram Adl <thenamesbram@gmail.com>
2022-06-15 14:24:35 +0700

    build: setup initial project architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
-import "./index.css";
+import App from './app/index.jsx';
+import "./app/index.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
